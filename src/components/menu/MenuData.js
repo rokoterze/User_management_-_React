12235f4568bb609234e3bb2e0eff504aa27c3850
Roko Terze <roko_terze@outlook.com>
2023-08-05 17:24:12 +0200

    05/08/2023

---
+++ src/components/menu/MenuData.js
@@ -0,0 +1,20 @@
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+export const MenuData = [
+    {
+        icon: <AddIcon/>,
+        link: "/addUser"
+    },
+
+    {
+        icon: <EditIcon/>,
+        link: "/editUser"
+    },
+
+    {
+        icon: <RemoveIcon/>,
+        link: "/removeUser"
+    }
+]
---
+++ src/components/sidebar/SidebarData.js
@@ -0,0 +1,17 @@
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+export const SidebarData = [
+    {
+        title: "Home",
+        icon: <HomeIcon/>,
+        link: "/home"
+    },
+
+    {
+        title: "Search User Database",
+        icon: <SearchIcon/>,
+        link: "/users"
+    }
+]
